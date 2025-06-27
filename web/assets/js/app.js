98c135adcba8ab1f6d36f5de4ebc0e20091a3096
maximopalopoli <mpalopoli@fi.uba.ar>
2025-06-27 18:21:01 -0300

    Add assets folder

---
+++ web/assets/js/app.js
@@ -0,0 +1,61 @@
+// If you want to use Phoenix channels, run `mix help phx.gen.channel`
+// to get started and then uncomment the line below.
+// import "./user_socket.js"
+
+// You can include dependencies in two ways.
+//
+// The simplest option is to put them in assets/vendor and
+// import them using relative paths:
+//
+//     import "../vendor/some-package.js"
+//
+// Alternatively, you can `npm install some-package --prefix assets` and import
+// them using a path starting with the package name:
+//
+//     import "some-package"
+//
+
+// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://650406f4e17ddceda64e4368937fba4c@o4508502393880576.ingest.de.sentry.io/4508518331449424",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/genesis\.alignedfoundation\.org\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+import "phoenix_html"
+import "./react/index"
+// Establish Phoenix Socket and LiveView configuration.
+// import {Socket} from "phoenix"
+// import {LiveSocket} from "phoenix_live_view"
+// import topbar from "../vendor/topbar"
+
+// let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+// let liveSocket = new LiveSocket("/live", Socket, {
+//   longPollFallbackMs: 2500,
+//   params: {_csrf_token: csrfToken}
+// })
+
+// Show progress bar on live navigation and form submits
+// topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+// window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
+// window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+
+// connect if there are any LiveViews on the page
+// liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
+// window.liveSocket = liveSocket

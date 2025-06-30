748e7050ca60f6777b06c9a6aed56828578335f5
maximopalopoli <mpalopoli@fi.uba.ar>
2025-06-30 12:14:43 -0300

    Add accounts, wallets and db migration for wallets

---
+++ web/priv/repo/migrations/20241218184947_create_wallets.exs
@@ -0,0 +1,14 @@
+defmodule AlignedAirdrop.Repo.Migrations.CreateWallets do
+  use Ecto.Migration
+
+  def change do
+    create table(:wallets, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :address, :string
+      add :amount, :string
+      add :merkle_proof, :text
+    end
+
+    create unique_index(:wallets, :address, name: :wallets_address_index)
+  end
+end

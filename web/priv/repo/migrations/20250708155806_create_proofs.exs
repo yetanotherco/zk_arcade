89e5818db35f17d9379ca29bbb3a167308c6a7ec
maximopalopoli <mpalopoli@fi.uba.ar>
2025-07-08 17:19:44 -0300

    Simplify migrations

--- web/priv/repo/migrations/20250708155806_create_proofs.exs
+++ web/priv/repo/migrations/20250708155806_create_proofs.exs
@@ -5,6 +5,7 @@ defmodule ZkArcade.Repo.Migrations.CreateProofs do
     create table(:proofs, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :verification_data, :map, null: false
+      add :batch_data, :map
       add :wallet_address, references(:wallets, column: :address, type: :string), null: false
 
       timestamps()

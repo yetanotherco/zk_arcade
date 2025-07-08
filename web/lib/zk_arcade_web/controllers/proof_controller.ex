cbb18786a9abfef1a88c4aca1c6443de5062f446
maximopalopoli <mpalopoli@fi.uba.ar>
2025-07-08 13:17:07 -0300

    Add the post method for the submit proof path

--- web/lib/zk_arcade_web/controllers/proof_controller.ex
+++ web/lib/zk_arcade_web/controllers/proof_controller.ex
@@ -5,4 +5,26 @@ defmodule ZkArcadeWeb.ProofController do
   def home(conn, _params) do
     render(conn, :home, layout: false)
   end
+
+  def submit(conn, %{
+        "verification_data" => verification_data_json,
+        "signature" => signature_json,
+        "address" => address,
+      }) do
+    with {:ok, verification_data} <- Jason.decode(verification_data_json),
+         {:ok, signature} <- Jason.decode(signature_json) do
+      Logger.info("Received verification_data: #{inspect(verification_data)}")
+      Logger.info("Received signature: #{inspect(signature)}")
+      Logger.info("Received address: #{inspect(address)}")
+
+
+    else
+      error ->
+        Logger.error("Error decoding JSON: #{inspect(error)}")
+
+        conn
+        |> put_flash(:error, "Invalid data received")
+        |> redirect(to: "/")
+    end
+  end
 end

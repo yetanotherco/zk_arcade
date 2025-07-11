752423f93c021b6f18a2ac743b5648fe5eed1fce
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-11 16:01:42 -0300

    feat: home hero + play games

--- web/lib/zk_arcade_web/components/core_components.ex
+++ web/lib/zk_arcade_web/components/core_components.ex
@@ -42,15 +42,44 @@ defmodule ZkArcadeWeb.CoreComponents do
 
         <div>
           <%!-- TODO: react component here --%>
-          <div class="bg-contrast-100 p-1 rounded flex justify-between items-center" style="width: 200px">
+          <div class="bg-contrast-100 p-1 rounded flex justify-between items-center" style="width: 180px">
             <div>
               <p class="text-xs">Connected:</p>
               <p class="font-bold text-md">0x032...3211</p>
             </div>
-            <.icon name="hero-chevron-down" class="size-3;5" />
+            <.icon name="hero-chevron-down" class="size-3.5" />
           </div>
         </div>
       </nav>
+    """
+  end
+
+  def line(assigns) do
+    ~H"""
+      <div class="w-full bg-accent-100" style="height: 1px"></div>
+    """
+  end
+
+  def section_header(%{header: header} = assigns) do
+    ~H"""
+      <div class="mb-10 size-fit">
+        <h2 class="mb-2 font-normal text-2xl size-fit" style="padding-right: 60px"><%= @header %></h2>
+        <.line />
+      </div>
+    """
+  end
+
+  def home_game_component(%{title: title, desc: desc, img: img} = assigns) do
+    ~H"""
+    <div class="cursor-pointer group">
+      <img class="rounded mb-5 w-full" src={@img} width={280} height={180} />
+      <div>
+        <h3 class="text-xl font-normal group-hover:underline underline-offset-4">
+          <%= @title %>
+        </h3>
+        <p class="text-md text-text-200"><%= @desc %></p>
+      </div>
+    </div>
     """
   end
 

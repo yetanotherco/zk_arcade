c7a51a03d2d4f6de3ec003ca16f55e9ee7c3718e
maximopalopoli <mpalopoli@fi.uba.ar>
2025-07-11 11:19:38 -0300

    Add todo coment on the verification data field

--- web/lib/zk_arcade/proofs/proof.ex
+++ web/lib/zk_arcade/proofs/proof.ex
@@ -6,6 +6,7 @@ defmodule ZkArcade.Proofs.Proof do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "proofs" do
+    # TODO: Store the commitments and th epub inputs instead of all the verification data
     field :verification_data, :map
     field :batch_data, :map
     belongs_to :wallet, Wallet, foreign_key: :wallet_address, references: :address, type: :string

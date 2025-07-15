2b3d5348f7048c1a638d5d323b1c0675a50e9722
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-15 10:25:32 -0300

    chore: increase backend length for json

--- web/lib/zk_arcade_web/endpoint.ex
+++ web/lib/zk_arcade_web/endpoint.ex
@@ -45,7 +45,9 @@ defmodule ZkArcadeWeb.Endpoint do
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+    json_decoder: Phoenix.json_library(),
+    length: 16_000_000
+
 
   plug Plug.MethodOverride
   plug Plug.Head

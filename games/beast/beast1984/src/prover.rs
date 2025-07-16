c014ad477bc54655b8c33f15cbc4602314c49c9f
Julian Arce <52429267+JuArce@users.noreply.github.com>
2025-07-16 17:59:24 -0300

    feat: commit address as public input (#14)
    * feat: commit address as public input
    
    * fix: update Makefile
    
    * fix: encoding

--- games/beast/beast1984/src/prover.rs
+++ games/beast/beast1984/src/prover.rs
@@ -1,3 +1,4 @@
+use alloy::hex;
 use game_logic::proving::{LevelLog, ProgramInput};
 use risc0_zkvm::{ExecutorEnv, ProverOpts, Receipt, default_prover};
 
@@ -12,11 +13,13 @@ pub enum ProvingError {
     SavingProof(String),
 }
 
-pub fn prove(levels_log: Vec<LevelLog>) -> Result<Receipt, ProvingError> {
+pub fn prove(levels_log: Vec<LevelLog>, address: String) -> Result<Receipt, ProvingError> {
     let mut env_builder = ExecutorEnv::builder();
 
+    let address_bytes = hex::decode(address)
+        .map_err(|e| ProvingError::WriteInput(e.to_string()))?;
     // write input data
-    let input = ProgramInput { levels_log };
+    let input = ProgramInput { levels_log, address: address_bytes };
     env_builder
         .write(&input)
         .map_err(|e| ProvingError::WriteInput(e.to_string()))?;

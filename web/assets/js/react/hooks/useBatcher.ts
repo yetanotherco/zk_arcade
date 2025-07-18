6470e08a06587123990ce9039805adf4f5d81037
maximopalopoli <mpalopoli@fi.uba.ar>
2025-07-18 16:41:07 -0300

    dont fetch nonce if addr is nil

--- web/assets/js/react/hooks/useBatcher.ts
+++ web/assets/js/react/hooks/useBatcher.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { encode as cborEncode, decode as cborDecode } from 'cbor-web';
 
-export function useBatcherNonce(host: string, port: number, address: string) {
+export function useBatcherNonce(host: string, port: number, address?: string) {
 	const [nonce, setNonce] = useState<`0x${string}` | null>(null);
 	const [error, setError] = useState<Error | null>(null);
 	const [loading, setLoading] = useState(true);
@@ -10,43 +10,45 @@ export function useBatcherNonce(host: string, port: number, address: string) {
 		let ws: WebSocket | null = null;
 
 		const fetchNonce = () => {
-			ws = new WebSocket(`ws://${host}:${port}`);
-			ws.binaryType = 'arraybuffer';
+			if (address) {
+				ws = new WebSocket(`ws://${host}:${port}`);
+				ws.binaryType = 'arraybuffer';
 
-			ws.onopen = () => {
-				console.log("WebSocket connection established");
-			};
+				ws.onopen = () => {
+					console.log("WebSocket connection established");
+				};
 
-			ws.onmessage = (event) => {
-				try {
-					const cbor_data = event.data;
-					const data = cborDecode(new Uint8Array(cbor_data));
+				ws.onmessage = (event) => {
+					try {
+						const cbor_data = event.data;
+						const data = cborDecode(new Uint8Array(cbor_data));
 
-					if (data?.ProtocolVersion) {
-						const message = { GetNonceForAddress: address };
-						const encoded = cborEncode(message).buffer;
-						ws?.send(encoded);
-					} else if (data?.Nonce) {
+						if (data?.ProtocolVersion) {
+							const message = { GetNonceForAddress: address };
+							const encoded = cborEncode(message).buffer;
+							ws?.send(encoded);
+						} else if (data?.Nonce) {
+							ws?.close();
+							setNonce(data.Nonce);
+							setLoading(false);
+						} else if (data?.EthRpcError || data?.InvalidRequest) {
+							ws?.close();
+							setError(new Error(JSON.stringify(data)));
+							setLoading(false);
+						}
+					} catch (e) {
 						ws?.close();
-						setNonce(data.Nonce);
-						setLoading(false);
-					} else if (data?.EthRpcError || data?.InvalidRequest) {
-						ws?.close();
-						setError(new Error(JSON.stringify(data)));
+						setError(e as Error);
 						setLoading(false);
 					}
-				} catch (e) {
-					ws?.close();
-					setError(e as Error);
-					setLoading(false);
-				}
-			};
+				};
 
-			ws.onerror = () => {
-				ws?.close();
-				setError(new Error("WebSocket connection error"));
-				setLoading(false);
-			};
+				ws.onerror = () => {
+					ws?.close();
+					setError(new Error("WebSocket connection error"));
+					setLoading(false);
+				};
+			}
 		};
 
 		fetchNonce();

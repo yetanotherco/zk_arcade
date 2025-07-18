e3852894dca124af5f14978414869c0539087ca2
maximopalopoli <mpalopoli@fi.uba.ar>
2025-07-18 17:32:16 -0300

    Use early return in case address is none in useBatcherNonce hook

--- web/assets/js/react/hooks/useBatcher.ts
+++ web/assets/js/react/hooks/useBatcher.ts
@@ -11,45 +11,47 @@ export function useBatcherNonce(host: string, port: number, address?: string) {
 		let ws: WebSocket | null = null;
 
 		const fetchNonce = () => {
-			if (address) {
-				ws = new WebSocket(`ws://${host}:${port}`);
-				ws.binaryType = 'arraybuffer';
+			if (!address) {
+				return
+			}
+			
+			ws = new WebSocket(`ws://${host}:${port}`);
+			ws.binaryType = 'arraybuffer';
 
-				ws.onopen = () => {
-					console.log("WebSocket connection established");
-				};
+			ws.onopen = () => {
+				console.log("WebSocket connection established");
+			};
 
-				ws.onmessage = (event) => {
-					try {
-						const cbor_data = event.data;
-						const data = cborDecode(new Uint8Array(cbor_data));
+			ws.onmessage = (event) => {
+				try {
+					const cbor_data = event.data;
+					const data = cborDecode(new Uint8Array(cbor_data));
 
-						if (data?.ProtocolVersion) {
-							const message = { GetNonceForAddress: address };
-							const encoded = cborEncode(message).buffer;
-							ws?.send(encoded);
-						} else if (data?.Nonce) {
-							ws?.close();
-							setNonce(hexToBigInt(data.Nonce));
-							setLoading(false);
-						} else if (data?.EthRpcError || data?.InvalidRequest) {
-							ws?.close();
-							setError(new Error(JSON.stringify(data)));
-							setLoading(false);
-						}
-					} catch (e) {
+					if (data?.ProtocolVersion) {
+						const message = { GetNonceForAddress: address };
+						const encoded = cborEncode(message).buffer;
+						ws?.send(encoded);
+					} else if (data?.Nonce) {
 						ws?.close();
-						setError(e as Error);
+						setNonce(hexToBigInt(data.Nonce));
+						setLoading(false);
+					} else if (data?.EthRpcError || data?.InvalidRequest) {
+						ws?.close();
+						setError(new Error(JSON.stringify(data)));
 						setLoading(false);
 					}
-				};
-
-				ws.onerror = () => {
+				} catch (e) {
 					ws?.close();
-					setError(new Error("WebSocket connection error"));
+					setError(e as Error);
 					setLoading(false);
-				};
-			}
+				}
+			};
+
+			ws.onerror = () => {
+				ws?.close();
+				setError(new Error("WebSocket connection error"));
+				setLoading(false);
+			};
 		};
 
 		fetchNonce();

3fa2716ba7374125d613ee2b85a47f6885fdd912
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-21 11:09:05 -0300

    feat: tags for game proving system and where it plays on

--- web/lib/zk_arcade_web/controllers/page_controller.ex
+++ web/lib/zk_arcade_web/controllers/page_controller.ex
@@ -56,6 +56,7 @@ defmodule ZkArcadeWeb.PageController do
         desc: "Survive across waves of enemies",
         full_desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus, felis sit amet convallis iaculis, felis purus commodo nibh, at sodales velit arcu a odio. Pellentesque dapibus volutpat odio, eu rutrum mauris malesuada et. Aliquam ligula velit, ultricies et mattis quis, ultrices in elit. Nam eget erat finibus, scelerisque purus eleifend, pretium lacus. Nam vitae tellus rhoncus, ornare libero eget, aliquam risus. Morbi lacinia lacinia ultricies. Morbi volutpat sollicitudin eros at vehicula. Pellentesque sed neque luctus, laoreet mi id, luctus est. Vivamus dictum ullamcorper lorem, non hendrerit purus condimentum et. Vestibulum viverra ligula vel lacinia porttitor. Donec blandit, ligula sit amet condimentum accumsan, quam elit sagittis nisl, et commodo lorem justo eget erat. Nam maximus arcu vel nibh feugiat accumsan. Ut aliquam massa ut pulvinar sagittis. Sed dictum mauris nec pretium feugiat. Aliquam erat volutpat. Mauris scelerisque sodales ex vel convallis.",
         how_to_play: "1. Download game base on your platform [here]\n2. Play game\n3. Locate the generated proof\n4. Upload your proof",
+        tags: [:cli, :risc0]
       })
       |> render(:game)
   end

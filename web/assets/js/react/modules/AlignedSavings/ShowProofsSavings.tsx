4423e321f3f6ed74f5386028b1e92cf156ad164d
maximopalopoli <mpalopoli@fi.uba.ar>
2025-07-22 14:44:18 -0300

    Improve the way costs are printed

--- web/assets/js/react/modules/AlignedSavings/ShowProofsSavings.tsx
+++ web/assets/js/react/modules/AlignedSavings/ShowProofsSavings.tsx
@@ -27,13 +27,13 @@ export default ({ proofs, proof_type, proofsPerBatch }: Props) => {
                     With {proofs} proofs of {proof_type}, the estimated savings are:
                 </p>
                 <p className="text-xl font-semibold">
-                    Base Cost: {calcAlignedSavings(proofs, proof_type, ethPrice, gasCostGwei, proofsPerBatch).baseCost} ETH
+                    Base Cost: {calcAlignedSavings(proofs, proof_type, ethPrice, gasCostGwei, proofsPerBatch).baseCost.toPrecision(6)} USD
                 </p>
                 <p className="text-xl font-semibold">
-                    Aligned Cost: {calcAlignedSavings(proofs, proof_type, ethPrice, gasCostGwei, proofsPerBatch).alignedCost} ETH
+                    Aligned Cost: {calcAlignedSavings(proofs, proof_type, ethPrice, gasCostGwei, proofsPerBatch).alignedCost.toPrecision(5)} USD
                 </p>
                 <p className="text-xl font-semibold">
-                    Savings: {calcAlignedSavings(proofs, proof_type, ethPrice, gasCostGwei, proofsPerBatch).savings} ETH
+                    Savings: {calcAlignedSavings(proofs, proof_type, ethPrice, gasCostGwei, proofsPerBatch).savings.toPrecision(6)} USD
                 </p>
                 <p className="text-sm text-gray-500">
                     Note: These calculations are based on current gas prices and may vary.

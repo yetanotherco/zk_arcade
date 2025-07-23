a05809305386d9fc500e4f237d944a75dcdb370e
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-23 12:01:42 -0300

    chore: remove unused code

--- web/assets/js/react/index.ts
+++ web/assets/js/react/index.ts
@@ -1,7 +1,6 @@
 import { define } from "remount";
 import Wallet from "./modules/Wallet";
 import SubmitProof from "./modules/SubmitProof/";
-import AlignedSavings from "./modules/AlignedSavings";
 
 define(
 	{ "x-app-user-wallet": Wallet },
@@ -19,9 +18,4 @@ define(
 define(
 	{ "x-app-submit-proof": SubmitProof },
 	{ attributes: ["network", "payment_service_address", "user_address", "batcher_host", "batcher_port"] }
-);
-
-define(
-	{ "x-app-aligned-savings": AlignedSavings },
-	{ attributes: ["proofs", "proof_type", "gas_cost_gwei", "proofsPerBatch"] }
 );

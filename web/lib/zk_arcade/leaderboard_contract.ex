23d4cafc91b0bca42ecc77b5906a52c42208ca0f
JuArce <52429267+JuArce@users.noreply.github.com>
2025-07-24 16:39:17 -0300

    fix: load leaderboard address in runtime instead of compile time

--- web/lib/zk_arcade/leaderboard_contract.ex
+++ web/lib/zk_arcade/leaderboard_contract.ex
@@ -2,22 +2,22 @@ defmodule ZkArcade.LeaderboardContract do
   require Logger
 
   use Ethers.Contract,
-    abi_file: "lib/abi/Leaderboard.json",
-    default_address: Application.get_env(:zk_arcade, :leaderboard_address)
+    abi_file: "lib/abi/Leaderboard.json"
 
   def top10 do
-    {:ok, top10_addresses} = get_top10_score() |> Ethers.call()
+    address = Application.get_env(:zk_arcade, :leaderboard_address)
+    {:ok, top10_addresses} = get_top10_score() |> Ethers.call(to: address)
 
-      top10_addresses
-      |> Enum.with_index(1)
-      |> Enum.map(fn {address, position} ->
-        {:ok, score} = users_score(address) |> Ethers.call()
+    top10_addresses
+    |> Enum.with_index(1)
+    |> Enum.map(fn {address, position} ->
+      {:ok, score} = users_score(address) |> Ethers.call(to: address)
 
-        %{
-          position: position,
-          address: address,
-          score: score
-        }
-      end)
+      %{
+        position: position,
+        address: address,
+        score: score
+      }
+    end)
   end
 end

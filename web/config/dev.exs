1677249fa4a66324506d95c13fe461ff44d2b2f4
Julian Arce <52429267+JuArce@users.noreply.github.com>
2025-07-24 17:31:11 -0300

    infra: add deploy config (#10)
    * infra: add deploy config
    
    * nit
    
    * fix: rename zk-arcade to zk_arcade
    
    * fix: missing install rust target
    
    * fix: instal rust target
    
    * fix: setcap path
    
    * fix: add missing variables to config files
    
    * fix: missing parenthesis
    
    * fix: typo
    
    * fix: database values
    
    * fix: add error_html controller
    
    * fix: load leaderboard address in runtime instead of compile time
    
    * fix: variable names

--- web/config/dev.exs
+++ web/config/dev.exs
@@ -83,7 +83,7 @@ config :phoenix_live_view,
   enable_expensive_runtime_checks: true
 
 config :zk_arcade, :payment_service_address, "0x7969c5ed335650692bc04293b07f5bf2e7a673c0"
-# Deploy the contract with make  make deploy_contract NETWORK=devnet and put the address here
+# Deploy the contract with `make deploy_contract NETWORK=devnet` and put the address here
 config :zk_arcade, :leaderboard_address, "<COMPLETE_ADDRESS_FROM_OUTPUT>"
 config :ethereumex,
   url: "http://localhost:8545"

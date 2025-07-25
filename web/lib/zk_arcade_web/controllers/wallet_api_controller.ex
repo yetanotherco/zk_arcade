7c2bdf9283ca6f89e28d8c26cd008fea0a2a9950
MauroFab <maurotoscano2@gmail.com>
2025-07-25 16:07:20 -0300

    Add missing newline at the end of files

--- web/lib/zk_arcade_web/controllers/wallet_api_controller.ex
+++ web/lib/zk_arcade_web/controllers/wallet_api_controller.ex
@@ -5,21 +5,21 @@ defmodule ZkArcadeWeb.WalletApiController do
     case ZkArcade.Accounts.fetch_wallet_by_address(String.downcase(address)) do
       {:ok, wallet} ->
         has_agreement = !is_nil(wallet.agreement_signature)
-        
+
         # If wallet has agreement, automatically create session
         conn = if has_agreement do
           put_session(conn, :wallet_address, wallet.address)
         else
           conn
         end
-        
+
         json(conn, %{
           exists: true,
           has_agreement: has_agreement,
           address: wallet.address,
           session_created: has_agreement
         })
-      
+
       {:error, :not_found} ->
         json(conn, %{
           exists: false,

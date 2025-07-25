3c820c38c64caaeba26c0db77b3d863cd8943076
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-25 09:24:48 -0300

    feat: game descriptions for sudoku and parity

--- web/lib/zk_arcade/utils.ex
+++ web/lib/zk_arcade/utils.ex
@@ -3,7 +3,7 @@ defmodule ZkArcade.Utils do
 
   def autolink_and_linebreak(text) do
     text
-    |> String.replace(@url_regex, ~s(<a href="\\0" target="_blank" rel="noopener noreferrer">\\0</a>))
+    |> String.replace(@url_regex, ~s(<a class="link" href="\\0" target="_blank" rel="noopener noreferrer">\\0</a>))
     |> String.replace("\n", "<br>")
   end
 

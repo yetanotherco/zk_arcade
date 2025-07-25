64ee0151e93f4e0c2b3bdd95bcbfdfc2ad354e67
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-25 08:36:26 -0300

    feat: acknowledgements in game

--- web/lib/zk_arcade_web/controllers/page_controller.ex
+++ web/lib/zk_arcade_web/controllers/page_controller.ex
@@ -59,6 +59,10 @@ defmodule ZkArcadeWeb.PageController do
   def game(conn, %{"name" => _game_name}) do
     wallet = get_wallet_from_session(conn)
     proofs = get_proofs(wallet)
+    acknowledgements = [
+      %{text: "Original Beast game repository", link: "https://github.com/dominikwilkowski/beast"},
+      %{text: "Original Beast game author", link: "https://github.com/dominikwilkowski"}
+    ]
 
     conn
       |> assign(:submitted_proofs, Jason.encode!(proofs))
@@ -69,6 +73,7 @@ defmodule ZkArcadeWeb.PageController do
         desc: "Survive across waves of enemies",
         full_desc: "The object of this arcade-like game is to survive through a number of levels while crushing the beasts (├┤) with movable blocks (░░). The beasts are attracted to the player's (◄►) position every move. The beginning levels have only the common beasts, however in later levels the more challenging super-beasts appear (╟╢). These super-beasts are harder to kill as they must be crushed against a static block (▓▓).",
         how_to_play: "1. Install Rust (https://www.rust-lang.org/tools/install).\n2. Install RiscZero toolchain (https://dev.risczero.com/api/zkvm/install)\n3. Clone zk_arcade repository with `git clone https://github.com/yetanotherco/aligned_layer.git`\n4. Play game with `make play_beast`\n5. Locate the generated proof\n6. Upload your proof",
+        acknowledgments: acknowledgements,
         tags: [:cli, :risc0]
       })
       |> render(:game)

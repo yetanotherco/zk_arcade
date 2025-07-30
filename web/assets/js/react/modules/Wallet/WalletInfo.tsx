fa1b746e4c14f15ecc24c74cc2c0b2e36ea1281b
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-07-30 12:38:35 -0300

    feat: user profile view

--- web/assets/js/react/modules/Wallet/WalletInfo.tsx
+++ web/assets/js/react/modules/Wallet/WalletInfo.tsx
@@ -45,9 +45,15 @@ export const WalletInfo = ({
 			</div>
 			<div className="pt-2">
 				<div
-					className="flex flex-col gap-8 p-8 absolute max-sm:left-0 sm:w-[400px] w-full sm:right-0 shadow-2xl bg-contrast-100 rounded opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-200 z-10"
-					style={{ maxHeight: 400 }}
+					className="flex flex-col gap-8 p-8 absolute max-sm:left-0 sm:w-[450px] w-full sm:right-0 shadow-2xl bg-contrast-100 rounded opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-opacity duration-200 z-10"
+					style={{ maxHeight: 450 }}
 				>
+					<div className="flex gap-2 items-center">
+						<span className="hero-user"></span>
+						<a href="/history" className="text-lg hover:underline">
+							Vitalik Buterin (#120)
+						</a>
+					</div>
 					<BalanceScoreInAligned
 						payment_service_address={payment_service_address}
 						leaderboard_address={leaderboard_address}

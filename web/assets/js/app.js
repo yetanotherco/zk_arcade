23aab0b64c4b366a0e25c3f7a62c6a6c28310ef2
maximopalopoli <mpalopoli@fi.uba.ar>
2025-08-08 15:55:51 -0300

    Move the tags to the components instead of using DOM

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,19 +1,2 @@
-// This fetches the links in the document and adds target="_blank" and rel="noopener noreferrer" to external
-// links. It works for external links that are not part of the current origin. It does not works for links
-// that are dynamically added after the page load, such as those created by React components.
-document.addEventListener("DOMContentLoaded", () => {
-  	document.querySelectorAll('a[href]').forEach(link => {
-		try {
-			const url = new URL(link.href, window.location.origin);
-			if (url.origin !== window.location.origin) {
-				link.setAttribute("target", "_blank");
-				link.setAttribute("rel", "noopener noreferrer");
-			}
-		} catch (e) {
-			console.warn("Invalid URL in link:", link.href);
-		}
-	});
-});
-
 import "phoenix_html";
 import "./react/index";

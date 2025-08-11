fa55f755f94980d4c1c296f1c326b830cc7ed454
Maximo Palopoli <96491141+maximopalopoli@users.noreply.github.com>
2025-08-11 13:58:56 -0300

    fix(web): open links in a new tab (#146)
    * Add the _blank, noopener noreferrer tags to make static links open in a new tab
    
    * Open the batch in the explorer URL in a new tab
    
    * Remove an extra import in hooks index.ts
    
    * Move the tags to the components instead of using DOM

--- web/assets/js/react/hooks/index.ts
+++ web/assets/js/react/hooks/index.ts
@@ -3,7 +3,6 @@ export * from "./useBatcherPaymentService";
 export * from "./useIsMounted";
 export * from "./useModal";
 export * from "./useOnClickOutside";
-export * from "./useOnClickOutside";
 export * from "./useBatcherNonce";
 export * from "./useLeaderboardContract";
 export * from "./useEthPrice";

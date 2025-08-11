0d04bc05243e373ebe4ecbf6b1c70c70fbdff15f
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-08-11 15:28:01 -0300

    feat: mantain the navbar position fixed

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,15 +1,3 @@
-// Used in nav to toggle the state on small screens
-function toggleMenu() {
-	const menuOverlay = document.querySelector("#menu-overlay");
-	const toggleOpen = document.querySelector(".toggle-open");
-	const toggleClose = document.querySelector(".toggle-close");
-
-	if (menuOverlay) menuOverlay.classList.toggle("hidden");
-	if (toggleOpen) toggleOpen.classList.toggle("hidden");
-	if (toggleClose) toggleClose.classList.toggle("hidden");
-}
-
-window.toggleMenu = toggleMenu;
-
 import "phoenix_html";
 import "./react/index";
+import "./navbar";

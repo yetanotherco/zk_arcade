bb0785d9a780190aa3e747ee5206d180295e0d54
maximopalopoli <mpalopoli@fi.uba.ar>
2025-08-13 12:58:00 -0300

    Hide the numeric input arrows on custom fee

--- web/assets/js/react/components/Modal/BumpFee.tsx
+++ web/assets/js/react/components/Modal/BumpFee.tsx
@@ -247,7 +247,7 @@ export const BumpFeeModal = ({
                             placeholder={`Enter fee > ${currentFeeEth} ETH`}
                             className={`w-full rounded-lg bg-contrast-100/10 px-3 py-2 outline-none disabled:opacity-50 ${
                                 choice === "custom" && customEth && !isCustomFeeInputValid ? "border border-red-400" : ""
-                            }`}
+                            } [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [-moz-appearance:textfield]`}
                             value={customEth}
                             onChange={(e) => {
                                 if (!isConfirmLoading) {

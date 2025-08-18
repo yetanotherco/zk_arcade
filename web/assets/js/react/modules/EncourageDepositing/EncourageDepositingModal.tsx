dd0df651623741f1544852b1fc5f99f3ce3303c7
maximopalopoli <mpalopoli@fi.uba.ar>
2025-08-18 13:49:19 -0300

    Format better the encourage depositing modal

--- web/assets/js/react/modules/EncourageDepositing/EncourageDepositingModal.tsx
+++ web/assets/js/react/modules/EncourageDepositing/EncourageDepositingModal.tsx
@@ -16,47 +16,47 @@ export const EncourageDepositingModal = ({payment_service_address, user_address}
 
     console.log("Modal state:", encourageOpen);
 
-  return (
-    <div>
-        <Modal
-            open={encourageOpen}
-            setOpen={setEncourageOpen}
-            maxWidth={600}
-        >
-                <div className="bg-contrast-100 p-10 rounded flex flex-col gap-8">
-                    <div className="text-center">
-                        <p className="text-text-100">
-                            To play zk-arcade, you need to deposit some funds into your wallet. This is necessary to submit your proofs to Aligned and claim your points in the LeaderBoard.
-                        </p>
+    return (
+        <div>
+            <Modal
+                open={encourageOpen}
+                setOpen={setEncourageOpen}
+                maxWidth={600}
+            >
+                    <div className="bg-contrast-100 p-10 rounded flex flex-col gap-8">
+                        <div className="text-center">
+                            <p className="text-text-100">
+                                To play zk-arcade, you need to deposit some funds into your wallet. This is necessary to submit your proofs to Aligned and claim your points in the LeaderBoard.
+                            </p>
+                        </div>
+
+                        <div className="text-center justify-between space-x-12">
+                            <Button
+                                variant="text"
+                                onClick={() => setEncourageOpen(false)}
+                                className="mt-6"
+                            >
+                                I'll deposit later
+                            </Button>
+
+                            <Button
+                                variant="accent-fill"
+                                onClick={() => { setEncourageOpen(false); setDepositOpen(true); }}
+                                className="mt-6"
+                            >
+                                Deposit funds now
+                            </Button>
+                        </div>
                     </div>
+            </Modal>
 
-                    <div className="text-center justify-between space-x-12">
-                        <Button
-                            variant="text"
-                            onClick={() => setEncourageOpen(false)}
-                            className="mt-6"
-                        >
-                            I'll deposit later
-                        </Button>
-
-                        <Button
-                            variant="accent-fill"
-                            onClick={() => { setEncourageOpen(false); setDepositOpen(true); }}
-                            className="mt-6"
-                        >
-                            Deposit funds now
-                        </Button>
-                    </div>
-                </div>
-        </Modal>
-
-        <DepositOnAlignedModal
-            payment_service_address={payment_service_address}
-            user_address={user_address}
-            open={depositOpen}
-            setOpen={setDepositOpen}
-        />
-    </div>
-  );
+            <DepositOnAlignedModal
+                payment_service_address={payment_service_address}
+                user_address={user_address}
+                open={depositOpen}
+                setOpen={setDepositOpen}
+            />
+        </div>
+    );
 };
 

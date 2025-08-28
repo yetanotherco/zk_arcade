72b68eeb47aad442d94152a728428118548f87be
maximopalopoli <mpalopoli@fi.uba.ar>
2025-08-28 18:02:32 -0300

    Add the merkle proof logic to the deploy contract script

--- contracts/scripts/deploy_contract.sh
+++ contracts/scripts/deploy_contract.sh
@@ -8,6 +8,7 @@ cd "$parent_path"
 # cd to contracts/
 cd ../
 
+# Deploy NFT Contract with Merkle Tree
 CMD="forge script script/deploy/NftContractDeployer.s.sol:NftContractDeployer \
     $NFT_CONFIG_PATH \
     $NFT_OUTPUT_PATH \
@@ -24,11 +25,21 @@ fi
 
 eval $CMD
 
+# Generate Merkle Proofs for frontend
+MERKLE_PROOFS_PATH="script/output/devnet/merkle-proofs.json"
+forge script script/deploy/GenerateMerkleProofs.s.sol:GenerateMerkleProofs \
+    $NFT_CONFIG_PATH \
+    $MERKLE_PROOFS_PATH \
+    --sig "run(string memory configPath, string memory outputFilePath)"
+
 # Obtain the proxy address and copy it to the leaderboard config
 nft_contract_proxy=$(jq -r '.addresses.proxy' $NFT_OUTPUT_PATH)
+merkle_root=$(jq -r '.merkle.root' $NFT_OUTPUT_PATH)
+
 jq --arg nft_contract_proxy "$nft_contract_proxy" '.zkArcadeNftContract = $nft_contract_proxy' $CONFIG_PATH > "script/deploy/config/devnet/leaderboard_temp.json"
 mv "script/deploy/config/devnet/leaderboard_temp.json" $CONFIG_PATH
 
+# Deploy Leaderboard
 CMD="forge script script/deploy/LeaderboardDeployer.s.sol:LeaderboardDeployer \
     $CONFIG_PATH \
     $OUTPUT_PATH \

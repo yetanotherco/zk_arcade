9d3b64d7c95a87dbc56ce801a6e6437a0887ac31
maximopalopoli <mpalopoli@fi.uba.ar>
2025-08-28 17:59:18 -0300

    Modify the Nft contract to accept the addresses merkle root

--- contracts/src/ZkArcadeNft.sol
+++ contracts/src/ZkArcadeNft.sol
@@ -8,23 +8,50 @@ import {ERC721URIStorageUpgradeable} from
     "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract ZkArcadeNft is ERC721URIStorageUpgradeable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 private _nextTokenId;
     mapping(address => bool) public minters;
 
+    bytes32 public merkleRoot;
+    mapping(address => bool) public hasClaimed;
+
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(address owner, string memory name, string memory symbol) public initializer {
+    function initialize(
+        address owner, 
+        string memory name, 
+        string memory symbol,
+        bytes32 _merkleRoot
+    ) public initializer {
         __ERC721_init(name, symbol);
         __Ownable_init(owner);
+        merkleRoot = _merkleRoot;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     function mint(string memory tokenURI) public onlyMinters(msg.sender) returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return tokenId;
+    }
+
+    function claimNFT(bytes32[] calldata merkleProof, string memory tokenURI) public returns (uint256) {
+        require(!hasClaimed[msg.sender], "NFT already claimed for this address");
+
+        // Verify that the address is whitelisted using Merkle Proof
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(MerkleProof.verify(merkleProof, merkleRoot, leaf), "Invalid merkle proof");
+
+        // Mark as claimed
+        hasClaimed[msg.sender] = true;
+
+        // Mint the NFT
         uint256 tokenId = _nextTokenId++;
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenURI);
@@ -42,6 +69,10 @@ contract ZkArcadeNft is ERC721URIStorageUpgradeable, UUPSUpgradeable, OwnableUpg
 
     function revokeMinter(address user) public onlyOwner {
         minters[user] = false;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
     }
 
     modifier onlyMinters(address user) {

9cdf67b40fa4c0ab7b595d7dce78a98d62244c89
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-09-08 15:10:08 -0300

    pause transfers by default

--- contracts/src/ZkArcadeNft.sol
+++ contracts/src/ZkArcadeNft.sol
@@ -8,6 +8,7 @@ import {ERC721URIStorageUpgradeable} from
     "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract ZkArcadeNft is ERC721URIStorageUpgradeable, UUPSUpgradeable, OwnableUpgradeable {
@@ -15,20 +16,22 @@ contract ZkArcadeNft is ERC721URIStorageUpgradeable, UUPSUpgradeable, OwnableUpg
 
     bytes32 public merkleRoot;
     mapping(address => bool) public hasClaimed;
+    bool transfersPaused = true;
 
     event MerkleRootUpdated(bytes32 indexed newRoot);
     event NFTClaimed(address indexed account);
+    event TransfersUnPaused();
+
+    error TransfersPaused();
 
     constructor() {
         _disableInitializers();
     }
 
-    function initialize(
-        address owner, 
-        string memory name, 
-        string memory symbol,
-        bytes32 _merkleRoot
-    ) public initializer {
+    function initialize(address owner, string memory name, string memory symbol, bytes32 _merkleRoot)
+        public
+        initializer
+    {
         __ERC721_init(name, symbol);
         __Ownable_init(owner);
         merkleRoot = _merkleRoot;
@@ -41,7 +44,7 @@ contract ZkArcadeNft is ERC721URIStorageUpgradeable, UUPSUpgradeable, OwnableUpg
 
         // Verify that the address is whitelisted using Merkle Proof
         bytes32 inner = keccak256(abi.encode(msg.sender));
-        bytes32 leaf  = keccak256(abi.encode(inner));
+        bytes32 leaf = keccak256(abi.encode(inner));
         require(MerkleProof.verify(merkleProof, merkleRoot, leaf), "Invalid merkle proof");
 
         // Mark as claimed
@@ -65,5 +68,21 @@ contract ZkArcadeNft is ERC721URIStorageUpgradeable, UUPSUpgradeable, OwnableUpg
         merkleRoot = _merkleRoot;
 
         emit MerkleRootUpdated(_merkleRoot);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override(ERC721Upgradeable, IERC721) {
+        if (transfersPaused) {
+            revert TransfersPaused();
+        }
+
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function setTransferPaused(bool _transfersPaused) public onlyOwner {
+        transfersPaused = _transfersPaused;
+
+        if (!transfersPaused) {
+            emit TransfersUnPaused();
+        }
     }
 }

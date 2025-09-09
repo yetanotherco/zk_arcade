379d6aa20671b91879d8152788ca9dac550c2ca8
maximopalopoli <mpalopoli@fi.uba.ar>
2025-09-09 10:33:47 -0300

    Fetch addresses from db and filter locally

--- merkle_tree/src/main.rs
+++ merkle_tree/src/main.rs
@@ -3,6 +3,8 @@ use merkle_tree_rs::standard::{LeafType, StandardMerkleTree};
 use serde::{Deserialize, Serialize};
 use sqlx::{Postgres, postgres::PgPoolOptions};
 use std::{env, fs};
+use std::collections::HashSet;
+use sqlx::Row;
 
 #[derive(Deserialize)]
 struct WhitelistWrapper {
@@ -24,9 +26,71 @@ struct Output {
     proofs: Vec<ProofEntry>,
 }
 
+async fn filter_addresses(addresses: Vec<String>) -> Result<Vec<String>, sqlx::Error> {
+    if dotenv().is_err() {
+        println!("Warning: No .env file found. Attempting to load .env.example");
+        let _ = from_filename(".env.example");
+    }
+
+    let database_url =
+        env::var("DATABASE_URL").expect("The environment variable DATABASE_URL is missing!");
+
+    let pool = PgPoolOptions::new()
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to the database!");
+    
+    let rows = sqlx::query(
+        r#"
+        SELECT address
+        FROM merkle_paths
+        WHERE lower(address) = ANY($1)
+        "#,
+    )
+    .bind(&addresses)
+    .fetch_all(&pool)
+    .await?;
+
+    let existing: HashSet<String> = rows
+        .into_iter()
+        .map(|r| r.get::<String, _>("address").to_lowercase())
+        .collect();
+
+    let to_insert: Vec<String> = addresses
+        .into_iter()
+        .filter(|a| !existing.contains(&a.to_lowercase()))
+        .collect();
+
+    println!("Already in DB: {existing:?}");
+    println!("New to insert: {to_insert:?}");
+
+    Ok(to_insert)
+}
+
 #[tokio::main]
 async fn main() {
     let args = env::args().skip(1).collect::<Vec<_>>();
+    if args.len() == 1 {
+        let in_path = &args[0];
+
+        println!("Processing input file: {}", in_path);
+        let data =
+            fs::read_to_string(in_path).unwrap_or_else(|e| panic!("Failed to read {in_path}: {e}"));
+        let parsed: WhitelistWrapper =
+            serde_json::from_str(&data).unwrap_or_else(|e| panic!("Invalid JSON: {e}"));
+
+        let addresses = parsed
+            .whitelist
+            .addresses
+            .into_iter()
+            .map(|a| a.to_lowercase().trim().to_string())
+            .collect::<Vec<_>>();
+
+        let filtered = filter_addresses(addresses.clone()).await.expect("Failed to filter addresses");
+
+        return;
+    }
+
     if args.len() != 3 {
         eprintln!("Usage: merkle_json_cli <input.json> <output.json> <merkle_root_index>");
         std::process::exit(1);

3938c07c660bdccec82c678570ce24a2e859e696
maximopalopoli <mpalopoli@fi.uba.ar>
2025-09-26 19:02:26 -0300

    Initial scaffolding

---
+++ test/stress_test.js
@@ -0,0 +1,13 @@
+import solution_file from './solution.json';
+
+// Read the solution from the solution.json file.
+const solution = solution_file;
+
+// Read the rich accounts addresses from the rich_accounts.json file.
+
+// Generate the proof verification data for each address
+
+// Make an HTTP post request to the backend with each address and its proof verification data
+
+// Measure the time taken for each request and log it to the console
+

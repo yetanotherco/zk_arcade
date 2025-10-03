5769e1d07d6cd2212fa320720396bd393f02f46f
Maximo Palopoli <96491141+maximopalopoli@users.noreply.github.com>
2025-10-03 11:24:58 -0300

    fix(contracts): use a safe reverse-loop pattern in getCurrentParityGame (#359)
    * fix(contracts): use a safe reverse-loop pattern in getCurrentParityGame
    
    * Save the current game in a var to avoid indexing many times
    
    * Apply suggestions from code review
    
    Co-authored-by: Julian Arce <52429267+JuArce@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Julian Arce <52429267+JuArce@users.noreply.github.com>

--- contracts/src/Leaderboard.sol
+++ contracts/src/Leaderboard.sol
@@ -278,9 +278,11 @@ contract Leaderboard is UUPSUpgradeable, OwnableUpgradeable {
     }
 
     function getCurrentParityGame() public view returns (ParityGame memory, uint256 idx) {
-        for (uint256 i = parityGames.length - 1; i >= 0; i--) {
-            if (block.timestamp >= parityGames[i].startsAtTime && block.timestamp < parityGames[i].endsAtTime) {
-                return (parityGames[i], i);
+        for (uint256 i = parityGames.length; i > 0; i--) {
+            uint256 j = i - 1;
+            ParityGame memory game = parityGames[j];
+            if (block.timestamp >= game.startsAtTime && block.timestamp < game.endsAtTime) {
+                return (game, j);
             }
         }
 

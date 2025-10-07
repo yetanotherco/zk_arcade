935b0f0f447a7ec4e95465478fec323b5c1dda98
maximopalopoli <mpalopoli@fi.uba.ar>
2025-10-07 17:51:09 -0300

    Change constants to match sepolia ones

--- test/constants.js
+++ test/constants.js
@@ -1,10 +1,10 @@
-import { anvil } from 'viem/chains'
+import { sepolia } from 'viem/chains'
 
-export const RPC_URL = 'http://localhost:8545'
-export const ZK_ARCADE_URL = "http://localhost:4005";
-export const BATCHER_URL = "http://localhost:8080";
+export const RPC_URL = 'https://ethereum-sepolia-rpc.publicnode.com'
+export const ZK_ARCADE_URL = "https://test.zkarcade.com";
+export const BATCHER_URL = "wss://sepolia.batcher.alignedlayer.com";
 
-export const BATCHER_PAYMENT_SERVICE_ADDR = '0x7bc06c482DEAd17c0e297aFbC32f6e63d3846650'
+export const BATCHER_PAYMENT_SERVICE_ADDR = "0x403dE630751e148bD71BFFcE762E5667C0825399"
 
-export const USED_CHAIN = anvil;
+export const USED_CHAIN = sepolia;
 

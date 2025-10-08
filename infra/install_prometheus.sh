84a1d789e8908545207f24a31487e202bc898b44
JuArce <52429267+JuArce@users.noreply.github.com>
2025-10-08 17:22:25 -0300

    fix: prometheus tar path

--- infra/install_prometheus.sh
+++ infra/install_prometheus.sh
@@ -2,7 +2,7 @@
 
 # This script installs Prometheus on a Linux system.
 wget -P ~/ https://github.com/prometheus/prometheus/releases/download/v3.6.0/prometheus-3.6.0.linux-amd64.tar.gz
-tar xvfz prometheus-*.tar.gz
+tar xvfz ~/prometheus-*.tar.gz -C ~/
 mkdir -p ~/config
 mkdir -p ~/.config/systemd/user/
 cp infra/prometheus.yaml ~/config/prometheus.yaml

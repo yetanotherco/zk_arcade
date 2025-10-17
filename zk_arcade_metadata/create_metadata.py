1c029f75212ab6f4b0f686efdad179663d81383f
MauroFab <maurotoscano2@gmail.com>
2025-10-17 17:17:29 -0300

    Fix metadata

--- zk_arcade_metadata/create_metadata.py
+++ zk_arcade_metadata/create_metadata.py
@@ -2,11 +2,12 @@ from pathlib import Path
 import json
 
 # ====== CONFIG ======
-COUNT = 10000   # how many tickets you want
-BASE_NAME = "Aligned ZK Arcade - Ticket - TESTNET ONLY"
+COUNT = 700   # how many tickets you want
+BASE_NAME = "Aligned ZK Arcade - Premium Ticket. TESTNET ONLY"
 DESCRIPTION = "Your ticket to the future of ethereum. TESTNET ONLY"
 IMAGE_URI = "ipfs://bafkreiabklbmsnqwhjktmz55i4kyk6efypf7565n7nfkbkpevcsfsujb6i"
 OUT_DIR = "."
+
 # ====================
 
 def make_metadata(name, description, image_uri):
@@ -19,7 +20,7 @@ def make_metadata(name, description, image_uri):
 outdir = Path(OUT_DIR) / "metadata"
 outdir.mkdir(parents=True, exist_ok=True)
 
-for token_id in range(1, COUNT + 1):
+for token_id in range(0, COUNT + 1):
     name = f"{BASE_NAME} #{token_id}"
     data = make_metadata(name, DESCRIPTION, IMAGE_URI)
     with (outdir / f"{token_id}").open("w", encoding="utf-8") as f:

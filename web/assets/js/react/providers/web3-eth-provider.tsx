68c4ef8929977bc3e9611ebdd0e33016561335c7
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-10-17 16:09:12 -0300

    remove args log error

--- web/assets/js/react/providers/web3-eth-provider.tsx
+++ web/assets/js/react/providers/web3-eth-provider.tsx
@@ -44,7 +44,6 @@ const Web3EthProvider = ({ children, network }) => {
 		const MATCH = /No matching key\. history:?/i;
 		const originalError = console.error;
 		console.error = (...args) => {
-			console.log("CONSOLE ERROR ARGS", args);
 			try {
 				const text = args
 					.map(a => (typeof a === "string" ? a : a?.message || ""))

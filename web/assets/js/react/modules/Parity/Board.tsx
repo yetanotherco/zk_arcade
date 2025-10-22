eace9c88fae94c24a6b8ae50f29e3b53ecebba69
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-10-22 17:47:55 -0300

    shorten text

--- web/assets/js/react/modules/Parity/Board.tsx
+++ web/assets/js/react/modules/Parity/Board.tsx
@@ -187,16 +187,9 @@ export const ParityBoard = ({
 					</Button>
 				</div>
 			</div>
-			<p className="text-xs text-center text-text-200 sm:text-left sm:w-auto w-full">
-				<span className="block sm:inline">
-					Use <span className="font-semibold">W</span>,{" "}
-					<span className="font-semibold">A</span>,{" "}
-					<span className="font-semibold">S</span>,{" "}
-					<span className="font-semibold">D</span> to move.
-				</span>
-				<span className="block sm:inline">
-					Press <span className="font-semibold">R</span> to reset.
-				</span>
+			<p className="text-sm text-center text-text-200 sm:text-left sm:w-auto w-full">
+				Move <span className="font-bold">[WASD]</span> - Reset{" "}
+				<span className="font-bold">[R]</span>
 			</p>
 		</div>
 	);

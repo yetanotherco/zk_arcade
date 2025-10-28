c8acee39d5bcd7b291e625315749db6b365ab463
JuArce <52429267+JuArce@users.noreply.github.com>
2025-10-28 17:11:46 -0300

    feat(web): show amount of addresses that claimed

--- web/lib/zk_arcade/proofs.ex
+++ web/lib/zk_arcade/proofs.ex
@@ -446,4 +446,13 @@ defmodule ZkArcade.Proofs do
 
     Logger.info("Broadcasted proof claimed notification for proof #{proof.id} by #{username || address}")
   end
+
+  def get_addresses_that_claimed_count() do
+    Proof
+    |> where([p], p.status == "claimed")
+    |> select([p], p.wallet_address)
+    |> distinct(true)
+    |> Repo.all()
+    |> length()
+  end
 end

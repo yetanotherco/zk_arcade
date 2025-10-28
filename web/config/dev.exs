de8b7e83352f8f5f494ffc35639c11985c439d10
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-10-28 16:13:03 -0300

    fix: date diffs convert to integer

--- web/config/dev.exs
+++ web/config/dev.exs
@@ -85,13 +85,13 @@ config :phoenix_live_view,
 config :zk_arcade, :payment_service_address, "0x7bc06c482DEAd17c0e297aFbC32f6e63d3846650"
 config :zk_arcade, :service_manager_address, "0x851356ae760d987E095750cCeb3bC6014560891C"
 # Deploy the contract with `make deploy_contract NETWORK=devnet` and put the address here
-config :zk_arcade, :leaderboard_address, "0x04C89607413713Ec9775E14b954286519d836FEf"
+config :zk_arcade, :leaderboard_address, "0x2E2Ed0Cfd3AD2f1d34481277b3204d807Ca2F8c2"
 config :ethereumex,
   url: "http://localhost:8545"
 
 config :zk_arcade, :network, "anvil"
 config :zk_arcade, :explorer_url, "http://localhost:4000"
-config :zk_arcade, :campaign_started_at, 1753031818
+config :zk_arcade, :campaign_started_at, "1761678316"
 
 config :zk_arcade, :batcher_host, "localhost"
 config :zk_arcade, :batcher_port, 8080
@@ -103,7 +103,7 @@ config :zk_arcade, :beast_windows_download_url, "https://github.com/yetanotherco
 config :zk_arcade, :beast_bash_download_url, "https://raw.githubusercontent.com/yetanotherco/zk_arcade/main/install_beast.sh"
 config :zk_arcade, :beast_bash_command, "beast"
 
-config :zk_arcade, :nft_contract_address, "0x1fA02b2d6A771842690194Cf62D91bdd92BfE28d"
+config :zk_arcade, :nft_contract_address, "0x4C4a2f8c81640e47606d3fd77B353E87Ba015584"
 
 
 config :zk_arcade, :ip_info_api_key, "<IP_INFO_API_KEY>"
